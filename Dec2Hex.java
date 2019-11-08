3c9c2d3806f0403eea1f9dccc6b37bfef2bf8cf8
DBRETT200 <DBRETT200@caledonian.ac.uk>
2019-11-08 18:06:39 +0000

    Java program improvement

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -20,12 +20,14 @@ class Dec2Hex
             }
         }
 
+if (Arg1 != null)
+{
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
 	num = Arg1;
 	String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
+}       
         while(num != 0)
         {
 	    rem=num%16;
@@ -34,6 +36,10 @@ class Dec2Hex
         }
         
         System.out.println("Hexadecimal representation is : " + hexadecimal);
-
+}
+else
+{
+ System.out.println("Error: No value to convert! Please enter a value")
+}
     }
 }
